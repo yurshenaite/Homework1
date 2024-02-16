9c6646cb37f7ff7383626d00394ff4a862144838
yurshenaite <159701852+yurshenaite@users.noreply.github.com>
2024-02-16 23:55:32 +0700

    13.py

--- 13.py
+++ 13.py
@@ -1,4 +1,4 @@
 blind_radius = int(input('Введите радиус слепой зоны: '))
 far_radius = int(input('Введите радиус дальности приема: '))
-territory_s = ((far_radius ** 2) * 3.14) - ((blind_radius ** 2) * 3.14)
-print(f'Площадь покрываемой территории равна {territory_s}')
+territory_s = abs(((far_radius ** 2) * 3.14) - ((blind_radius ** 2) * 3.14))
+print(f'Площадь покрываемой территории равна {round(territory_s, 1)}')
